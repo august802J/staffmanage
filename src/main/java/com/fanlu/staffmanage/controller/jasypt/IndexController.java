de06a39ed5c839ed542b7640fe734d9af11db181
YGwhere <ygwhere@gmail.com>
2021-02-07 17:37:14 +0800

    略

--- src/main/java/com/fanlu/staffmanage/controller/jasypt/IndexController.java
+++ src/main/java/com/fanlu/staffmanage/controller/jasypt/IndexController.java
@@ -1,10 +1,8 @@
-package com.fanlu.staffmanage.jasypt;
+package com.fanlu.staffmanage.controller.jasypt;
 
 import org.jasypt.encryption.StringEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
